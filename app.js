7d8e59fcb57d194ce8928a938d25273503ccc2a3
DawidKurkiewicz <dawidkurkiewicz516@gmail.com>
2019-07-25 14:49:19 +0200

    capture

--- app.js
+++ app.js
@@ -2,4 +2,6 @@ const divs = document.querySelectorAll("div")
 function logText(e){
     console.log(this.classList.value)
 }
-divs.forEach(div => div.addEventListener("click", logText))
+divs.forEach(div => div.addEventListener("click", logText, {
+    capture: true
+}))
