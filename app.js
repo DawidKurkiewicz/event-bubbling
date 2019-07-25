a8f897b74b36034cdca2bf874d0a824b2ef85126
DawidKurkiewicz <dawidkurkiewicz516@gmail.com>
2019-07-25 14:50:27 +0200

    stop propagation

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const divs = document.querySelectorAll("div")
 function logText(e){
+    e.stopPropagation()
     console.log(this.classList.value)
 }
-divs.forEach(div => div.addEventListener("click", logText, {
-    capture: true
-}))
+divs.forEach(div => div.addEventListener("click", logText))
